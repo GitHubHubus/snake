079a6524abe489bcb7a64bb7f04b21129a7f6b14
Kochetkov Oleg <oleg.kochetkov999@yandex.ru>
2020-05-19 07:16:32 +0300

    changed mailer params

--- server/mailer.js
+++ server/mailer.js
@@ -5,7 +5,8 @@ var smtpTransport = require('nodemailer-smtp-transport');
 var transporter = nodemailer.createTransport(smtpTransport({
     service: settings.mailer.service,
     host: settings.mailer.host,
-    port: 587,
+    port: 465,
+    secure: true,
     auth: {
         user: settings.mailer.user,
         pass: settings.mailer.pass
